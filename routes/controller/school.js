81401b4ce0b66f818aeb976f71df3e8526c7da43
mohit prakash <mohit.prakash@inmobi.com>
2019-05-17 23:28:53 +0530

    add school

--- routes/controller/school.js
+++ routes/controller/school.js
@@ -1,23 +1,50 @@
 var express = require("express");
 var router = express.Router();
 const pool = require("../../config/config");
+var jsonInsertQuery = require("../../utils/common");
 
-router.post("/add-school", (request, response) => {
-  pool.query(
-    `insert into school_details
-    (school_name, school_address1, school_address2) values ('${request.body.name}','${
-      request.body.password
-    }','${request.body.role}')`,
-    (error, result) => {
+router
+  .route("/")
+  .post((request, response) => {
+    pool.query(
+      jsonInsertQuery("school_details", request.body),
+      (error, result) => {
+        if (error) throw error;
+
+        if (result.affectedRows > 0) {
+          response.statusCode = 201;
+          response.json({
+            success: 1,
+            message: "School inserted successfully"
+          });
+        } else {
+          response.statusCode = 409;
+          response.send("Something went wrong... Try again");
+        }
+      }
+    );
+  })
+  .get((request, response) => {
+    pool.query(`select * from school_details`, (error, result) => {
       if (error) throw error;
 
-      if (result.affectedRows > 0) {
-        response.send("Data inserted successfully");
+      if (result.length > 0) {
+        response.statusCode = 200;
+        response.json({
+          success: 1,
+          data: result
+        });
       } else {
-        response.send("Something went wrong... Try again");
+        response.statusCode = 404;
+        response.send("School not found");
       }
-    }
-  );
+    });
+  });
+
+router.route("/change-status/:schoolId").put((request, response) => {
+  if(request.params.schoolId === 'APPROVED'){
+    
+  }
 });
 
 module.exports = router;
